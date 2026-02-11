5c2b504a49a7e94d4c9498bffc2af92272eb2dd3
sosog <sosogvritishvili@gmail.com>
2026-02-11 12:27:26 +0400

    add example for parcel.js

---
+++ examples/vite/src/translations/es.ts
@@ -0,0 +1,22 @@
+export const es = {
+    global: {
+        title: 'Demostración de Translit'
+    },
+    user: {
+        greeting: '¡Hola, {{name}}!',
+        roles: {
+            admin: 'Administrador'
+        }
+    },
+    messages: {
+        unread: 'Tienes un mensaje no leído.',
+        unread_plural: 'Tienes {{count}} mensajes no leídos.'
+    },
+    items: {
+        cart: 'Un artículo en el carrito',
+        cart_plural: '{{count}} artículos en el carrito'
+    },
+    html: {
+        welcome: 'Bienvenido, <b>{{name}}</b>!'
+    }
+};
